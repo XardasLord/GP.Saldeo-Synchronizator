be692620a34ebca8ed4f1aea4b29b8160534fc8a
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-07-12 18:21:46 +0200

    Created first DB migration

---
+++ GP.SS.Database/Migrations/20190712161632_CreateCompanyTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GP.SS.Database.Migrations
+{
+    public partial class CreateCompanyTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Saldeo.Companies",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CompanyProgramId = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    ShortName = table.Column<string>(nullable: true),
+                    FullName = table.Column<string>(nullable: true),
+                    VatNumber = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Postcode = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: true),
+                    Telephone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Saldeo.Companies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Saldeo.Companies");
+        }
+    }
+}
