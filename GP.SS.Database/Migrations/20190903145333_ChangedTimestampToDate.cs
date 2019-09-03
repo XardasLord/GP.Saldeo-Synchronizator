b38d3a5e5ccbf6dc2347bb85ef786dc7a529f2c7
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2019-09-03 17:02:19 +0200

    Created migration to change timestamp to date

---
+++ GP.SS.Database/Migrations/20190903145333_ChangedTimestampToDate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GP.SS.Database.Migrations
+{
+    public partial class ChangedTimestampToDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DATE_SALE",
+                table: "SALDEO_DOCUMENTS",
+                type: "DATE",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DATE_RECEIVE",
+                table: "SALDEO_DOCUMENTS",
+                type: "DATE",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DATE_PAYMENT",
+                table: "SALDEO_DOCUMENTS",
+                type: "DATE",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DATE_ISSUE",
+                table: "SALDEO_DOCUMENTS",
+                type: "DATE",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DATE_SALE",
+                table: "SALDEO_DOCUMENTS",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "DATE",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DATE_RECEIVE",
+                table: "SALDEO_DOCUMENTS",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "DATE",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DATE_PAYMENT",
+                table: "SALDEO_DOCUMENTS",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "DATE",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DATE_ISSUE",
+                table: "SALDEO_DOCUMENTS",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "DATE",
+                oldNullable: true);
+        }
+    }
+}
